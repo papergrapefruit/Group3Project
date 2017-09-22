83c0c57226978409b7d0648148fe59a02e0d24ae
Adam Haag <adam.m.h@gmail.com>
2017-09-22 13:36:59 -0400

    Randomly generated pieces can now be saved to db

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,5 +4,14 @@ module.exports = function(app){
   app.get("api/scores/:userId", function(req, res){
     var user = req.params.userId;
     console.log(user);
+  });
+
+  app.post("/api/save/random", function(req, res){
+    db.Score.create({
+      title: req.body.title,
+      music: req.body.music
+    }).then(function(dbPost){
+      res.json(dbPost);
+    })
   })
 }
